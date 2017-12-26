13fc5d93f74ac3a95a6bd7f9c621ee1be3dcdde4
Wenceslao Negrete <vampay@digitaldealers.mx>
2017-12-26 01:41:07 -0600

    First gh-pages push - 5

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('./sw.js')
+  .then(function(reg){
+    console.log('Successfully registered service worker', reg);
+  })
+  .catch(function(err) {
+    console.warn('Error whilst registering service worker', err);
+  });
+}
+
 ReactDOM.render(<App />, 
   document.getElementById('root'));
